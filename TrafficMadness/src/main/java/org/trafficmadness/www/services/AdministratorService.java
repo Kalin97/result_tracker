f2832645b52b081cec6fcebf9a8aac1cc7b100a8
Kalin97 <kalo150@abv.bg>
2016-03-16 17:44:47 +0200

    Administrator service, closes #14.

---
+++ TrafficMadness/src/main/java/org/trafficmadness/www/services/AdministratorService.java
@@ -0,0 +1,74 @@
+package org.trafficmadness.www.services;
+
+import javax.persistence.EntityManager;
+
+import org.trafficmadness.www.user.entities.Administrator;
+
+public class AdministratorService extends Service
+{
+	public AdministratorService(EntityManagerService entityManagerService) 
+	{
+		super(entityManagerService);
+	}
+
+	public Administrator getData(long id)
+	{
+		final EntityManager em = entityManagerService.createEntityManager();
+		try 
+		{
+			final Administrator result = em.find(Administrator.class, id);
+			if (result == null) 
+			{
+				throw new IllegalArgumentException("No news with id: " + id);
+			}
+			
+			return result;
+		} 
+		finally 
+		{
+			em.close();
+		}
+	}
+
+	public Administrator addData(Administrator administrator)
+	{
+		final EntityManager em = entityManagerService.createEntityManager();
+		try 
+		{
+			em.getTransaction().begin();
+			em.persist(administrator);
+			em.getTransaction().commit();
+			
+			return administrator;
+		} 
+		finally 
+		{
+			if (em.getTransaction().isActive()) 
+			{
+				em.getTransaction().rollback();
+			}
+			em.close();
+		}
+	}
+	
+	public Administrator updateData(Administrator administrator)
+	{				
+		final EntityManager em = entityManagerService.createEntityManager();
+		try 
+		{
+			em.getTransaction().begin();
+			em.merge(administrator);
+			em.getTransaction().commit();
+			
+			return administrator;
+		} 
+		finally 
+		{
+			if (em.getTransaction().isActive()) 
+			{
+				em.getTransaction().rollback();
+			}
+			em.close();
+		}
+	}
+}
