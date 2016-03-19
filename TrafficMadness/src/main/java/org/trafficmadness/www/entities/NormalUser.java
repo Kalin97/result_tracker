73b608ddb4a8b65edfc871c103d274f340da494b
Kalin97 <kalo150@abv.bg>
2016-03-19 12:20:48 +0200

    Add friend list to Player, closes #10.

--- TrafficMadness/src/main/java/org/trafficmadness/www/entities/NormalUser.java
+++ TrafficMadness/src/main/java/org/trafficmadness/www/entities/NormalUser.java
@@ -1,5 +1,7 @@
 package org.trafficmadness.www.entities;
 
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -26,6 +30,13 @@ public class NormalUser implements IUser
 	@OneToOne(optional = false, cascade = CascadeType.ALL)
 	@JoinColumn(name="PLAYER_ID", unique=true, nullable=false, updatable=false)
 	private Player player;
+	
+	@JoinTable(
+		joinColumns = {	@JoinColumn(name = "user", referencedColumnName = "NORMALUSER_ID", nullable = false) }, 
+		inverseJoinColumns = {	@JoinColumn(name = "friend", referencedColumnName = "NORMALUSER_ID", nullable = false) }
+	)
+	@ManyToMany
+	private List<NormalUser> friends;
 
 	public long getId() 
 	{
@@ -47,6 +58,16 @@ public class NormalUser implements IUser
 		this.player = player;
 	}
 
+	public List<NormalUser> getFriends() 
+	{
+		return friends;
+	}
+
+	public void setFriends(List<NormalUser> friends) 
+	{
+		this.friends = friends;
+	}
+	
 	@Override
 	public boolean isAdministrator() 
 	{
